37d8b03e6526cc333bcac205b06efddde9aeacd4
yash7xm <yash.7xm@gmail.com>
2024-02-22 17:34:13 +0530

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-slate-500">
-      Hello world!
-    </h1>
+        Hello world!
+      </h1>
+      <Button>Clicke Me</Button>
     </>
   );
 }
