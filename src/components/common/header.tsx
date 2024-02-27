c24df09767635728772e3e0398bfdc6afa668e89
yash7xm <yash.7xm@gmail.com>
2024-02-27 19:25:02 +0530

    header centered

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -4,10 +4,10 @@ import { useNavigate } from "react-router-dom";
 const Header = ({ headerType }: any) => {
   const navigate = useNavigate();
   return (
-    <div className="p-3 font-mono font-700 uppercase text-xs border-b stretch">
+    <div className="text-center md:text-start p-3 font-mono font-700 uppercase text-xs border-b stretch">
       {headerType == "Project Detail" ? (
         <div
-          className="flex justify-start items-center gap-2 w-fit cursor-pointer"
+          className="flex justify-center md:justify-start items-center gap-2 w-fit cursor-pointer"
           onClick={() => navigate(-1)}
         >
           <span>
@@ -15,9 +15,9 @@ const Header = ({ headerType }: any) => {
           </span>
           <span>See All Projects</span>
         </div>
-      ) : 
-      headerType == "About" ? (<span>About Me</span>) :
-      (
+      ) : headerType == "About" ? (
+        <span>About Me</span>
+      ) : (
         <span> Selected Work</span>
       )}
     </div>
