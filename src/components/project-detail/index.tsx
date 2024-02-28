c2477085ce66e8316544d1b7884ae8b66eb87629
yash7xm <yash.7xm@gmail.com>
2024-02-28 13:33:32 +0530

    project expand mobile style added

--- src/components/project-detail/index.tsx
+++ src/components/project-detail/index.tsx
@@ -10,40 +10,43 @@ const ProjectDetailComponent = () => {
   }
 
   return (
-    <div className="scroll-style overflow-y-scroll p-3">
+    <div className="scroll-style overflow-y-scroll p-3 h-full">
       {/* main image */}
-      <div className="h-screen w-full flex items-center justify-center dark:bg-secondary-bg bg-[#33333312] rounded-md">
-        <div
-          className="h-4/5 w-4/5 bg-contain rounded-md"
-          style={{ backgroundImage: `url(${data[idx].mainImage})` }}
-        ></div>
+      <div className="h-2/5 md:h-full w-full flex items-center justify-center dark:bg-secondary-bg bg-[#33333312] rounded-md">
+        <div className="size-4/5 rounded-md">
+          <img
+            className="rounded-md size-full"
+            src={data[idx].mainImage}
+            alt="project image"
+          ></img>
+        </div>
       </div>
 
       {/* key point descriptions */}
-      <div className="dark:bg-secondary-bg bg-sec h-3/5 mt-4 rounded-md p-4 flex flex-col justify-between">
+      <div className="dark:bg-secondary-bg bg-sec h-fit md:h-3/5 mt-4 rounded-md p-4 flex flex-col justify-between">
         {/* heading */}
-        <div className="text-xl uppercase w-4/5 font-mono">
+        <div className="text-xl uppercase w-full md:w-4/5 font-mono mb-20 md:mb-0">
           <span>{data[idx].name} </span>
           <span className="text-muted-foreground">{data[idx].description}</span>
         </div>
 
         {/* summary-techstack-links */}
-        <div className="grid grid-cols-3 gap-8 font-mono">
+        <div className="grid md:grid-cols-3 gap-8 font-mono">
           <div>
-            <div className="mb-3 uppercase text-sm">Summary</div>
-            <div className="dark:text-[#FFFFFFB3] text-[#303030B3] text-xs">
+            <div className="mb-3 uppercase text-lg md:text-sm">Summary</div>
+            <div className="dark:text-[#FFFFFFB3] text-[#303030B3] text-sm md:text-xs">
               {data[idx].summary}
             </div>
           </div>
           <div>
-            <div className="mb-3 uppercase text-sm">Tech Stack</div>
-            <div className="dark:text-[#FFFFFFB3] text-[#303030B3] text-xs">
+            <div className="mb-3 uppercase text-lg md:text-sm">Tech Stack</div>
+            <div className="dark:text-[#FFFFFFB3] text-[#303030B3] text-sm md:text-xs">
               {data[idx].techStack}
             </div>
           </div>
           <div>
-            <div className="mb-3 uppercase text-sm">Links</div>
-            <div className="dark:text-[#FFFFFFB3] text-[#303030B3] text-xs flex flex-col">
+            <div className="mb-3 uppercase text-lg md:text-sm">Links</div>
+            <div className="dark:text-[#FFFFFFB3] text-[#303030B3] text-sm md:text-xs flex flex-col">
               <a href={data[idx].gitLink}>Github</a>
               <a href={data[idx].webLink}>Live</a>
             </div>
@@ -53,20 +56,26 @@ const ProjectDetailComponent = () => {
 
       {/* project-info */}
       {data[idx].projectInfo.map((info, index) => (
-        <div key={index}>
-          <div className="h-screen w-full flex items-center mt-4 justify-center dark:bg-secondary-bg bg-[#33333312] rounded-md">
-            <div
-              className="h-4/5 w-4/5 bg-contain rounded-md"
-              style={{ backgroundImage: `url(${info.url})` }}
-            ></div>
+        <div key={index} className="h-fit my-8">
+          {/* image */}
+          <div className="h-2/5 md:h-full w-full flex items-center mt-4 justify-center dark:bg-secondary-bg bg-[#33333312] rounded-md">
+            <div className="size-full md:size-4/5 bg-contain rounded-md">
+              <img
+                className="rounded-md size-full"
+                src={info.url}
+                alt="project image"
+              ></img>
+            </div>
           </div>
-          <div className="h-fit py-24 w-full flex justify-end pr-24">
-            <div className="w-2/5 flex flex-col gap-3">
+
+          {/* text */}
+          <div className="h-fit p-3 md:py-24 w-full md:flex justify-end md:pr-24 my-4">
+            <div className="md:w-2/5 flex flex-col gap-3">
               <div className="text-lg uppercase">{info.head}</div>
               {info.body.map((bdy, index) => (
                 <div
                   key={index}
-                  className="font-mono text-xs text-start dark:text-[#FFFFFFB3] text-[#303030B3]"
+                  className="font-mono text-sm md:text-xs text-start dark:text-[#FFFFFFB3] text-[#303030B3]"
                 >
                   {bdy}
                 </div>
