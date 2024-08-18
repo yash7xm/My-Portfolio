35fd9621472a7d80fe2105ab11f40f30a036edf3
yash7xm <yash.7xm@gmail.com>
2024-08-18 23:10:45 +0530

    contacts added

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -4,9 +4,15 @@ const sepratorStyle = "text-muted-foreground mx-2";
 const Footer = () => {
   return (
     <div className="p-3 font-mono font-700 uppercase text-xs md:border-t text-center md:text-start w-full">
-      <span className={textStyle}>Email</span>
+      <span className={textStyle}>
+        <a href="mailto:yash.7xm@gmail.com">Email</a>
+      </span>
       <span className={sepratorStyle}>/</span>
-      <span className={textStyle}>Linkedin</span>
+      <span className={textStyle}>
+        <a href="https://www.linkedin.com/in/yash-poonia-00326022b/">
+          Linkedin
+        </a>
+      </span>
       <span className={sepratorStyle}>/</span>
       <span className={textStyle}>
         <a href="../../public/yashpoonia.pdf" download="yashpoonia.pdf">
@@ -14,7 +20,13 @@ const Footer = () => {
         </a>
       </span>
       <span className={sepratorStyle}>/</span>
-      <span className={textStyle}>Twitter</span>
+      <span className={textStyle}>
+        <a href="https://x.com/Yash_Poonia_">Twitter</a>
+      </span>
+      <span className={sepratorStyle}>/</span>
+      <span className={textStyle}>
+        <a href="https://github.com/yash7xm">GitHub</a>
+      </span>
     </div>
   );
 };
