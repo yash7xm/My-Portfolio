c75f8fd88459796426745b85b5c604f63529f843
yash7xm <yash.7xm@gmail.com>
2024-10-14 18:44:40 +0530

    resume update

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -2,33 +2,38 @@ const textStyle = "cursor-pointer";
 const sepratorStyle = "text-muted-foreground mx-2";
 
 const Footer = () => {
-  return (
-    <div className="p-3 font-mono font-700 uppercase text-xs md:border-t text-center md:text-start w-full">
-      <span className={textStyle}>
-        <a href="mailto:yash.7xm@gmail.com">Email</a>
-      </span>
-      <span className={sepratorStyle}>/</span>
-      <span className={textStyle}>
-        <a href="https://www.linkedin.com/in/yash-poonia-00326022b/">
-          Linkedin
-        </a>
-      </span>
-      <span className={sepratorStyle}>/</span>
-      <span className={textStyle}>
-        <a href="../../public/yashpoonia.pdf" download="yashpoonia.pdf">
-          Resume
-        </a>
-      </span>
-      <span className={sepratorStyle}>/</span>
-      <span className={textStyle}>
-        <a href="https://x.com/Yash_Poonia_">Twitter</a>
-      </span>
-      <span className={sepratorStyle}>/</span>
-      <span className={textStyle}>
-        <a href="https://github.com/yash7xm">GitHub</a>
-      </span>
-    </div>
-  );
+    return (
+        <div className="p-3 font-mono font-700 uppercase text-xs md:border-t text-center md:text-start w-full">
+            <span className={textStyle}>
+                <a href="mailto:yash.7xm@gmail.com">Email</a>
+            </span>
+            <span className={sepratorStyle}>/</span>
+            <span className={textStyle}>
+                <a href="https://www.linkedin.com/in/yash-poonia-00326022b/">
+                    Linkedin
+                </a>
+            </span>
+            <span className={sepratorStyle}>/</span>
+            <span className={textStyle}>
+                <a
+                    href="../../public/yashpoonia.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Resume
+                </a>
+            </span>
+
+            <span className={sepratorStyle}>/</span>
+            <span className={textStyle}>
+                <a href="https://x.com/Yash_Poonia_">Twitter</a>
+            </span>
+            <span className={sepratorStyle}>/</span>
+            <span className={textStyle}>
+                <a href="https://github.com/yash7xm">GitHub</a>
+            </span>
+        </div>
+    );
 };
 
 export default Footer;
